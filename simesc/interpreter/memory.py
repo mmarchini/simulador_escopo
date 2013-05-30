1e1d94a7a8eade12437ae24340373533b4c2d856
Matheus Marchini <matheusdot@gmail.com>
2013-05-30 20:33:40 -0300

    la

--- simesc/interpreter/memory.py
+++ simesc/interpreter/memory.py
@@ -1,14 +1,66 @@
 from abc import ABCMeta, abstractmethod, abstractproperty
 from stack import Stack
 
-class MemoryStack(object):
-    pass
+class MemoryFrame(object):
+    __callback__ = -1
+    __function_name__ = ""
+    __variables__ = []
+
+    ###############
+    # Constructor #
+    ###############    
+    
+    def __init__(self, callback, function_name):
+        self.callback = callback
+        self.function_name = function_name
+    
+    ###########
+    # Getters #
+    ###########
+    
+    @property
+    def callback(self):
+        return self.__callback__
+
+    @property
+    def function_name(self):
+        return self.__function_name__
+        
+    @property
+    def variables(self):
+        return self.__variables__
+
+    ###########
+    # Setters #
+    ###########
+
+    @callback.setter
+    def callback(self, value):
+        self.__callback__ = value
+
+    @function_name.setter
+    def function_name(self, value):
+        self.__function_name__ = value
+        
+    @variables.setter
+    def variables(self, value):
+        self.__variables__ = value
 
 class MemoryReference(object):
-    pass
+    __type__ = None
+    __value__ = None
+    
+    def __init__(self, type, value=0):
+        pass
+    
+class Variable(object):
+    __name__ = ""
+    __type__ = None
+    __value__ = None
 
 class Memory(object):
     __metaclass__ = ABCMeta
+    
     ##############
     # Attributes #
     ##############
@@ -16,14 +68,15 @@ class Memory(object):
     __code__ = []
     __stack__ = None
     __mount__ = None
+    __variables__ = []
     
     ###############
     # Constructor #
     ###############
     
     def __init__(self, code=None):
-        self.__stack__ = Stack(MemoryStack)
-        self.__mount__ = Stack(MemoryReference)
+        self.stack = Stack(MemoryFrame)
+        self.mount = Stack(MemoryReference)
         
         self.code = code
         
